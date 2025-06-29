6635c431cf489f4362062893dc610ec75aa20261
Curtis Atchley <curtis@curtisatchley.com>
2025-06-29 18:13:07 -0500

    Updated ReCAPTCHA and added Google notice to footer

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -4,6 +4,25 @@ export default function Footer() {
       <p className="font-black">Curtis Atchley</p>
       <p>Web developement in central IL.</p>
       <p className="text-sm">&copy; Curtis Atchley 2025</p>
+      <p className="text-xs text-gray-500 mt-2 text-center">
+        This site is protected by reCAPTCHA and the Google{" "}
+        <a
+          href="https://policies.google.com/privacy"
+          target="_blank"
+          className="underline"
+        >
+          Privacy Policy
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://policies.google.com/terms"
+          target="_blank"
+          className="underline"
+        >
+          Terms of Service
+        </a>{" "}
+        apply.
+      </p>
     </footer>
   );
 }
